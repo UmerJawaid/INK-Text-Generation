6152ff8418b3aa6e051f458de9abc4ac3d3c867d
umer jawaid <umerjawaid@umers-MBP.sudofy.com>
2021-03-30 17:16:32 +0500

    Validate data on image caption page

--- cypress/integration/Image Captions.js
+++ cypress/integration/Image Captions.js
@@ -1,18 +1,6 @@
 
 beforeEach(() => {
-    cy.visit("https://testing.inkforall.com/tools", {
-        onLoad: (contextwin) => {
-            var b = document.getElementsByTagName('button')
-            for (let i = 0; i < b.length; i++) {
-                b[i].id = `ink_button_${i}`;
-            }
-            var t = document.getElementsByTagName('textarea')
-            for (let i = 0; i < b.length; i++) {
-                b[i].id = `ink_textarea_${i}`;
-            }
-        }
-    });
-});
+    cy.visit('https://testing.inkforall.com/tools')});
 describe('Visit Image Captains page, validate sample out,place holder,heading and validate text field.', function () {
     it('Visits text field on this page and validate the fields.', function () {
         cy.get('#item-1-dropdown').invoke('show');
@@ -20,6 +8,17 @@ describe('Visit Image Captains page, validate sample out,place holder,heading an
         cy.url().should('eq', 'https://testing.inkforall.com/tools/twitter-image-caption/');
         cy.get('textarea') && cy.get('textarea').type('one two three', { force: true });
         cy.get('.formButton',{force:true}).click()
-        cy.get('.googleButton').click()
+        // cy.get('.googleButton').click()
+        cy.visit('https://testing.inkforall.com/tools/twitter-image-caption/?code=4ds34s-231sed2-123sde-32s2332')
+        cy.wait(10000)
+        cy.get('textarea') && cy.get('textarea').type('Too ', { force: true });
+        cy.get('.formButton',{force:true}).click()
+        cy.get('.MuiAlert-message').contains("More text required!").invoke('text').then((text)=>{let toastText = text;
+        expect(toastText).to.equal("More text required!");})
+        cy.get('textarea') && cy.get('textarea').type('Too excited to put this one in writing but we’re doing a little shout out to our friends at Product Hunt who voted our product as their product of the month. We’re honored to be this month’s pick!', { force: true });
+        cy.get('.formButton',{force:true}).click()  
+        cy.waitFor('.marketing-banner')
+        cy.get('.marketing-banner > p').should('have.text','You got the perfect Twitter content. Now what?')
+        cy.get('.resultHeader > p').should('have.text','RESULTS')
     })
 })
